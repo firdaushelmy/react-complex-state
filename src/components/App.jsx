301a75f0e35d741edfe5b20b46c5622638753e8b
Firdaus Helmy <46524543+firdaushelmy@users.noreply.github.com>
2020-08-06 23:43:41 +0800

    add useState for name change

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,43 @@
-import React from "react";
+import React, { useState } from 'react';
 
 function App() {
+  const [firstNameValue, setFirstNameValue] = useState('');
+  const [lastNameValue, setLastNameValue] = useState('');
+
+  const [firstSubmitValue, setFirstSubmitValue] = useState('');
+  const [lastSubmitValue, setLastSubmitValue] = useState('');
+
+  function changeHandler(event) {
+    setFirstNameValue(event.target.value);
+    setLastNameValue(event.target.value);
+    console.log(event.target);
+  }
+
+  function clickHandler(event) {
+    setFirstSubmitValue(firstNameValue);
+    setLastSubmitValue(lastNameValue);
+    event.preventDefault();
+  }
+
   return (
-    <div className="container">
-      <h1>Hello</h1>
+    <div className='container'>
+      <h1>
+        Hello {firstSubmitValue} {lastSubmitValue}
+      </h1>
       <form>
-        <input name="fName" placeholder="First Name" />
-        <input name="lName" placeholder="Last Name" />
-        <button>Submit</button>
+        <input
+          name='fName'
+          placeholder='First Name'
+          value={fistName}
+          onChange={changeHandler}
+        />
+        <input
+          name='lName'
+          placeholder='Last Name'
+          value={fistName}
+          onChange={changeHandler}
+        />
+        <button onClick={clickHandler}>Submit</button>
       </form>
     </div>
   );
